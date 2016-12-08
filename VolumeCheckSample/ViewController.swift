64772cdc4b80a7a660458c1f40fa8746da0110ee
SUZUKIyuka <yuka.suzuki1218@gmail.com>
2016-12-08 13:45:39 +0900

    Initial Commit

---
+++ VolumeCheckSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VolumeCheckSample
+//
+//  Created by nagata on 12/8/16.
+//  Copyright © 2016 nagata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
